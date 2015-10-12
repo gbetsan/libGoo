22540520561e42522159f4eb7e6abe73115e0ea5
Grig O. Betsan <griggors@gmail.com>
2015-10-12 22:31:20 +0300

    modified:   lib/libGoo/objects.rb

--- lib/libGoo/objects.rb
+++ lib/libGoo/objects.rb
@@ -1,43 +1,52 @@
 module LibGoo
   class ObjectProcessor #Much needed such wow, really, idk why it's here
-    attr_reader :id, :params
+    attr_reader :params
 
     def initialize(params, raise_if)
       if params.is_a?(Hash) || params.is_a?(JSON)
         raise LibGooError, "No specified key #{raise_if} in params"  unless params.has_key?(raise_if) && raise_if
         @params = params
-        #params.each_pair do |k,v|
-        #  instance_variable_set '@'+k.to_s,v
-        #end
-      elsif params.is_a?(Fixnum)
-        @id = params
       else
-        raise LibGooError, "Unavailable type of argument: #{params.class.to_i}."
+        raise LibGooError, "Unavailable type of argument: #{params.class.to_s}."
       end
     end
 
     def method_missing(m, *args)
+      return self.get_var(m) if self.get_var(m)
+      super
+    end
+
+    def get_var(m)
       if @params.has_key?(m.to_s)
         @params[m.to_s]
       elsif @params.has_key?(m)
         @params[m]
       else
-        super
+        false
       end
+    end
+
+    def self.descendants
+      ObjectSpace.each_object(Class).select { |klass| klass < self }
+    end
+
+    def self.parent
+      LibGoo::ObjectProcessor
     end
   end
 
   class Victim < ObjectProcessor
+    @@important = 'user_id'
     def initialize(params)
-      super(params, 'user_id')
+      super(params, @@important)
+    end
+
+  end
+
+  class Processes < ObjectProcessor
+    @@important = 'id'
+    def initialize(params)
+      super(params, @@important)
     end
   end
 end
-
-require 'pp'
-include LibGoo
-#u = ObjectProcessor.new(vasya: 1, tolya: 3, lolik: 2)
-#pp u.lolik
-us = Victim.new(lol: 1, 'user_id' => 1001)
-puts us.lol
-puts us.user_id
